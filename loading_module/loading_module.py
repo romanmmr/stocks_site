cb5bc5d515754ead30de8d48f21112b645631b26
rodri <rodrigo.roman.mm@gmail.com>
2024-05-23 17:38:52 +0200

    Create multi-notebook site, and preprocessing for arima model

--- loading_module/loading_module.py
+++ loading_module/loading_module.py
@@ -4,29 +4,30 @@ import yfinance as yf
 
 class LoadTickers:
 
-    def __init__(self, ticker='^IBEX', period='max', weeks=12, debug=False):
+    def __init__(self, ticker='^IBEX', period='max', display_weeks=12, update_data=False):
         self.ticker = ticker
         self.period = period
-        self.weeks = weeks
-        self.debug = debug
+        self.display_weeks = display_weeks
+        self.update_data = update_data
         self.ticker_values = pd.DataFrame()
-        self.display_values = pd.DataFrame()
+        self.display_table = pd.DataFrame()
 
     def load_tickers(self) -> pd.DataFrame:
         """
         Load historical values of the selected ticker and period
         """
-        if self.debug:
+        if self.update_data:
+            self.ticker_values = yf.Tickers(self.ticker).tickers[self.ticker].history(period=self.period)
+            self.ticker_values.to_csv('loading_module/ticker_values.csv')
+        else:
             self.ticker_values = pd.read_csv('loading_module/ticker_values.csv')
             self.ticker_values.set_index('Date', inplace=True)
             self.ticker_values.index = pd.to_datetime(self.ticker_values.index, utc=True)
-        else:
-            self.ticker_values = yf.Tickers(self.ticker).tickers[self.ticker].history(period=self.period)
 
         self.ticker_values = self.ticker_values.resample('W-FRI').first().sort_values(by=['Date'])
 
     def get_display_table(self):
-        self.display_values = self.ticker_values.tail(self.weeks)['Close'].to_frame().T
+        self.display_table = self.ticker_values.tail(self.display_weeks)['Close'].to_frame().T
 
     def run_pipline(self):
         self.load_tickers()
@@ -38,10 +39,10 @@ if __name__ == '__main__':
 
     ticker = '^IBEX'
     period = 'max'
-    weeks = 12
+    display_weeks = 12
     debug = True
 
-    tick = LoadTickers(ticker=ticker, period=period, weeks=weeks, debug=debug)
+    tick = LoadTickers(ticker=ticker, period=period, display_weeks=display_weeks, update_data=debug)
     tick.run_pipline()
 
     print(tick.ticker_values)
--- models/arima.py
+++ models/arima.py
@@ -1,4 +1,5 @@
 # import os
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -8,40 +9,94 @@ from loading_module.loading_module import LoadTickers
 
 
 class Arima:
-    def __init__(self, ticker: str, period: int, weeks: int, debug: bool):
+    def __init__(
+            self,
+            ticker: str,
+            period: int,
+            display_weeks: int,
+            test_weeks: int,
+            freq: str,
+            column: str,
+            years: float,
+            update_data: bool,
+    ):
         self.ticker = ticker
         self.period = period
-        self.weeks = weeks
-        self.debug = debug
+        self.display_weeks = display_weeks
+        self.test_weeks = test_weeks
+        self.freq = freq
+        self.column = column
+        self.log_column = f"log_{column}"
+        self.years = years
+        self.update_data = update_data
         self.ticker_values = pd.DataFrame
+        self.modelling_data = pd.DataFrame
+        self.train = pd.DataFrame
+        self.test = pd.DataFrame
+        self.train_index = None
+        self.test_index = None
 
     def load_values(self) -> None:
         values = LoadTickers(
             ticker=self.ticker,
             period=self.period,
-            weeks=self.weeks,
-            debug=self.debug
+            display_weeks=self.display_weeks,
+            update_data=self.update_data
         )
         values.load_tickers()
         self.ticker_values = values.ticker_values
-        # if self.debug:
-        #     print(self.ticker_values)
+
+    def get_modelling_data(self) -> None:
+        self.modelling_data = self.ticker_values[self.column][-52*self.years:].copy(deep=True).to_frame()
+
+    def get_log_column(self) -> None:
+        self.modelling_data[self.log_column] = np.log(self.modelling_data[self.column])
+
+    def define_freq(self) -> None:
+        self.modelling_data.freq = self.freq
+
+    def define_train_test(self) -> None:
+        self.train = self.modelling_data.iloc[:-self.test_weeks]
+        self.test = self.modelling_data.iloc[-self.test_weeks:]
+
+        self.train_index = self.modelling_data.index <= self.train.index[-1]
+        self.test_index = self.modelling_data.index > self.train.index[-1]
+
+    def run_preprocess(self) -> None:
+        self.load_values()
+        self.get_modelling_data()
+        self.get_log_column()
+        self.define_freq()
+        self.define_train_test()
+
+    # def define_arima_model(self):
+
+
+
+    # TODO: ARIMA - order... fit...
 
 if __name__ == '__main__':
-    print('hello')
 
     ticker = '^IBEX'
     period = 'max'
-    weeks = 12
-    debug = True
+    display_weeks = 12
+    test_weeks = 8
+    column = 'Close'
+    years = 4
+    update_data = False
+    freq = 'W-FRI'
 
-    arima = Arima(ticker=ticker, period=period, weeks=weeks, debug=debug)
-    arima.load_values()
+    arima = Arima(
+        ticker=ticker,
+        period=period,
+        display_weeks=display_weeks,
+        test_weeks=test_weeks,
+        freq=freq,
+        column=column,
+        years=years,
+        update_data=update_data
+    )
+    arima.run_preprocess()
     print(arima.ticker_values.head())
 
-    # csv_test_path = 'C:\\Users\\rodri\\PycharmProjects\\stocks_website\\docs\\test_csv.csv'
-    # ticker_values = pd.read_csv(csv_test_path)
-    #
-    # ticker_values.set_index('Date', inplace=True)
-    #
-    # print('done')
+    print('done')
