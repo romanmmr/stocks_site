e4d331a5b7548cafa2c5d0a23f5165073bbef8db
rodri <rodrigo.roman.mm@gmail.com>
2024-06-15 22:14:11 +0200

    Add NN model - back and front

--- docs/display_data.py
+++ docs/display_data.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
 
 
 def plot_sample_data(path: str, column: str) -> None:
@@ -27,7 +28,7 @@ def table_horizontal(path: str, column: str, display_weeks: int) -> None:
 
 
 
-def plot_fit_and_forecast(path: str, column: str, display_weeks: int) -> None:
+def plot_fit_and_forecast(path: str, column: str, model: str, display_weeks: int) -> None:
     modelling_data = pd.read_csv(path, index_col='Date')
     # modelling_data.index = pd.to_datetime(modelling_data.index).strftime('%Y-%m-%d')
     modelling_data.index = pd.to_datetime(modelling_data.index, utc=True)
@@ -40,16 +41,16 @@ def plot_fit_and_forecast(path: str, column: str, display_weeks: int) -> None:
     )
     ax.plot(
         # modelling_data['arima_output'].tail(display_weeks),
-        modelling_data['arima_log_output'].tail(display_weeks),
+        modelling_data[f"{model}_output"].tail(display_weeks),
         color='green',
-        label='model output - fitted'
+        label=f"{model} model output - fitted"
     )
     ax.fill_between(
         modelling_data.tail(display_weeks).index,
         # modelling_data['arima_conf_int_lower'].tail(display_weeks),
         # modelling_data['arima_conf_int_upper'].tail(display_weeks),
-        modelling_data['arima_log_conf_int_lower'].tail(display_weeks),
-        modelling_data['arima_log_conf_int_upper'].tail(display_weeks),
+        modelling_data[f"{model}_conf_int_lower"].tail(display_weeks),
+        modelling_data[f"{model}_conf_int_upper"].tail(display_weeks),
         color='red',
         alpha=0.3,
         label='confidence interval'
@@ -59,7 +60,7 @@ def plot_fit_and_forecast(path: str, column: str, display_weeks: int) -> None:
     plt.tight_layout()
     plt.legend()
 
-def plot_predictions(path: str, column: str, display_weeks: int) -> None:
+def plot_predictions(path: str, column: str, model: str, display_weeks: int) -> None:
     predicted_data = pd.read_csv(path, index_col='Date')
     # modelling_data.index = pd.to_datetime(modelling_data.index).strftime('%Y-%m-%d')
     predicted_data.index = pd.to_datetime(predicted_data.index, utc=True)
@@ -67,16 +68,16 @@ def plot_predictions(path: str, column: str, display_weeks: int) -> None:
 
     fig, ax = plt.subplots(figsize=(10, 5))
     ax.plot(
-        predicted_data[column].tail(display_weeks),
-        label='real data + predictions'
+        predicted_data[f"{column}_{model}"].tail(display_weeks),
+        label=f"{model} real data + predictions"
     )
 
     ax.fill_between(
         predicted_data.tail(display_weeks).index,
         # modelling_data['arima_conf_int_lower'].tail(display_weeks),
         # modelling_data['arima_conf_int_upper'].tail(display_weeks),
-        predicted_data['arima_log_conf_int_lower'].tail(display_weeks),
-        predicted_data['arima_log_conf_int_upper'].tail(display_weeks),
+        predicted_data[f"{model}_conf_int_lower"].tail(display_weeks),
+        predicted_data[f"{model}_conf_int_upper"].tail(display_weeks),
         color='red',
         alpha=0.3,
         label='confidence interval'
@@ -88,7 +89,26 @@ def plot_predictions(path: str, column: str, display_weeks: int) -> None:
 
 # if __name__ == '__main__':
 #     print('start')
-#     plot_sample_data(path='arima_fitted_data.csv', column='Close')
-#     table_horizontal(path='arima_fitted_data.csv', column='Close', display_weeks=16)
-#     plot_fit_and_forecast(path='arima_fitted_data.csv', column='Close', display_weeks=16)
+#     plot_sample_data(path='modelled_fitted_data.csv', column='Close')
+#     table_horizontal(path='modelled_fitted_data.csv', column='Close', display_weeks=16)
+#     plot_fit_and_forecast(path='modelled_fitted_data.csv', column='Close', display_weeks=16)
 #     print('done')
+
+def plot_nn_loss(path:str) -> None:
+
+    my_path = ''
+    for _ in os.getcwd().split('\\')[:-1]:
+        my_path += _ + '\\'
+
+    history = pd.read_csv(os.path.join(my_path, path))
+
+    plot_loss = history['loss']
+    plot_val_loss = history['val_loss']
+    plot_epochs = range(len(plot_loss))
+
+    fig_mae = plt.figure(figsize=(10, 5))
+    plt.plot(plot_epochs, plot_loss, 'b', label='Training loss (mae)')
+    plt.plot(plot_epochs, plot_val_loss, 'r', label='Validation loss (mae)')
+    plt.title('Training and Validation loss (mae)')
+    plt.grid()
+    plt.legend()
